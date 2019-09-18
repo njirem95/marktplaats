f25c0438d5cfbd2606d0e49a9620f1b6ce06cd46
Merijn Kruithof <merijnkruithof@hotmail.com>
2019-09-18 12:29:24 +0200

    chat commands

--- src/services/ChatService/src/events/ChatMessageEvent.ts
+++ src/services/ChatService/src/events/ChatMessageEvent.ts
@@ -4,11 +4,36 @@ import * as socketio from "socket.io";
 export class ChatMessageEvent implements Event {
     constructor(private _socket: socketio.Server) { }
 
+    /**
+     * Handles the incoming chat message.
+     *
+     * It also checks if data is a chat command instead of a regular
+     * chat message.  
+     *  
+     * @param session
+     * @param data 
+     */
     public handle(session: socketio.Socket, data?: any): void {
         // TODO Validate data properly.
         if (data.length == 0) return;
 
+        if (this.isChatCommand(data)) {
+            // TODO Handle chat command.
+            return;
+        }
+
         // TODO Pass to an outbound handler instead of emitting directly using _socket.
         this._socket.emit('chat message', data);
+    }
+
+    /**
+     * Checks if message is a chat command.
+     * 
+     * message is a chat command if it  with a "!".
+     * 
+     * @param message the incoming message
+     */
+    private isChatCommand(message: string): boolean {
+        return message.startsWith("!");
     }
 }
--- src/services/ChatService/src/events/DisconnectEvent.ts
+++ src/services/ChatService/src/events/DisconnectEvent.ts
@@ -5,6 +5,6 @@ export class DisconnectEvent implements Event {
     constructor(private _server: socketio.Server) {}
 
     public handle(session: socketio.Socket, data?: any): void {
-        this._server.emit('chat message', `${session.handshake.address} with id ${session.id} left the server.`);
+        this._server.emit('chat message', `Someone left the server.`);
     }
 }
