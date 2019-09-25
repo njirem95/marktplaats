48d2e4f9361b390c89bda993196ae04a5b410f5c
Merijn Kruithof <merijnkruithof@hotmail.com>
2019-09-25 13:25:07 +0200

    nothing interesting has happened

--- src/desktop/electron-chatmaster/main.js
+++ src/desktop/electron-chatmaster/main.js
@@ -2,15 +2,15 @@ const { app, BrowserWindow } = require('electron');
 
 function createWindow() {
     let window = new BrowserWindow({
-        width: 1200,
-        height: 1000,
+        width: 400,
+        height: 600,
         webPreferences: {
             nodeIntegration: true   
         }
     });
 
     window.loadFile('index.html');
-    window.webContents.openDevTools();
+    //window.webContents.openDevTools();
 }
 
 app.on('ready', createWindow);
